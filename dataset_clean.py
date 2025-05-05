9d10359fbcaf95ab38231b6a99ec3fa64e56b6b5
Nolan Denson-Childers <143834259+dcChilshock@users.noreply.github.com>
2025-05-05 15:32:34 +0000

    update

--- dataset_clean.py
+++ dataset_clean.py
@@ -1,5 +1,18 @@
 import pandas as pd
 import numpy as np
 data_str = "/workspaces/electricvehicle_databasics/top-1000-trending-youtube-videos copy.csv"
-pd.read_csv(data_str,index=["rank","Video","Video views","Likes","Dislikes","Category","published"])
+index =["rank","Video","Video views","Likes","Dislikes","Category","published"]
+df = pd.read_csv(data_str)
+length = 6
+#for i in
 #search through and handle missing values
+value = df[1:2]
+print(value)
+print(df[0:2])
+def process():
+    pass
+
+
+#func search/sort?
+#mean / median / mode / standard deviation  
+#value of views / likes / dislikes / date published

5d102d595286184e1e9502163a0c38e7f320a99b
Nolan Denson-Childers <143834259+dcChilshock@users.noreply.github.com>
2025-05-14 15:49:16 +0000

    update

--- dataset_clean.py
+++ dataset_clean.py
@@ -3,27 +3,22 @@ import numpy as np
 data_str = "/workspaces/electricvehicle_databasics/top-1000-trending-youtube-videos copy.csv"
 index =["rank","Video","Video views","Likes","Dislikes","Category","published"]
 df = pd.read_csv(data_str)
-#value_list = df[1:2].values.tolist() #IMPORTANT
-#print(value_list)
-#print(df.loc[df["rank"]> 50])\
-#print(df["rank"])
 
 def process(df): #search through and analise the data
     index =["rank","Video","Video views","Likes","Dislikes","Category","published"]
     ll = len(index)
-    #to clean - all
-    #to analyze - views, likes, dislikes, category, and publish
     i = 0
     x = 0
-    like = 0
-    dislike = 0
+    like = [0,0]
+    dislike = [0,0]
+    view = [0,0]
     suml = 0
     sumd = 0
     sumv = 0
     miss = 0
     categories = []
     for x in range(0,ll):
-        indexlist = ["rank","Video views","Likes","Dislikes","published"]
+        indexlist = ["rank","Video views","Likes","Dislikes","published"] #use this to determine if this value needs to be int or str.
         ranklist = df[index[x]].values.tolist()
         l = len(ranklist)
         for i in range(0,l):
@@ -36,26 +31,35 @@ def process(df): #search through and analise the data
             if a == "nan":
                 if index[x] == "Video":
                     df.remove(i)
-                    l -= 1 
-                if index[x] == "Video views"
-                ranklist[i:i+1] = ["Miss"]  #Handle missing values here, and clean data set
+                    l = l - 1 
+                if index[x] == "Video views":
+                    ranklist[i:i+1] = [0]
                 miss += 1
                 i += 1
+                if index[x] == "Likes":
+                    ranklist[i:i+1] = [0]
+                if index[x] == "Dislikes":
+                    ranklist[i:i+1] = [0]
+                if index[x] == "published":
+                    ranklist[i:i+1] = [0]
             else:
+                if index[x] == "rank":
+                    rank = ranklist[i]
                 if index[x] == "Category":
                     categories.append(ranklist[i])
                 if index[x] == "Likes": #Sort and make data here
-                    u = int(a)
-                    suml += u
-                    if int(u) > like:
-                        like = int(u)
+                    suml += int(a)
+                    if int(a) > like[0]:
+                        like = [int(a),df.loc[int(rank)-1]]
                 if index[x] == "Dislikes":
-                    u = int(a)
-                    sumd += u
-                    if int(u) > dislike:
-                        dislike = int(a)
+                    sumd += int(a)
+                    if int(a) > dislike[0]:
+                        dislike = [int(a),df.loc[int(rank)-1]]
                 if index[x] == "Video views":
                     sumv += int(a)
+                    if int(a) > view[0]:
+                        view = [int(a),df.loc[int(rank)-1]]
+
                 i += 1
         x += 1
     md = sumd / l
@@ -64,18 +68,6 @@ def process(df): #search through and analise the data
     print(mv)
     print(ml)
     print(md)
-    #lenn = len(ranklist)
-    #print(str(lenn))
-    #df['Likes'] = pd.Series(ranklist) 
-    #print("This is the mean amount of likes across all videos:")
-    #print(ranklist)
-    #print(df)
-    #print(sum1)
+    print(df.loc[5])
+    print(view)
 process(df)
-
-#idx.str.rstrip()
-#Out[31]: Index([' jack', 'jill', ' jesse', 'frank'], dtype='object')
-
-#func search/sort?
-#mean / median / mode / standard deviation  
-#value of views / likes / dislikes / date published

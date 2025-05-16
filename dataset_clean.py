61c50eadf851dc8dd6d3452258ee7806408321ce
Nolan Denson-Childers <143834259+dcChilshock@users.noreply.github.com>
2025-05-16 15:32:05 +0000

    update

--- dataset_clean.py
+++ dataset_clean.py
@@ -9,6 +9,9 @@ def process(df): #search through and analise the data
     ll = len(index)
     i = 0
     x = 0
+    c = 0
+    cc = 0
+    ccc = 0
     like = [0,0]
     dislike = [0,0]
     view = [0,0]
@@ -33,41 +36,69 @@ def process(df): #search through and analise the data
                     df.remove(i)
                     l = l - 1 
                 if index[x] == "Video views":
+                    pass 
                     ranklist[i:i+1] = [0]
                 miss += 1
                 i += 1
                 if index[x] == "Likes":
+                    pass
                     ranklist[i:i+1] = [0]
                 if index[x] == "Dislikes":
+                    pass
                     ranklist[i:i+1] = [0]
                 if index[x] == "published":
+                    pass
                     ranklist[i:i+1] = [0]
+                if index[x] == "Category":
+                    pass
+                    ranklist[i:i+1] = ["na"]
             else:
                 if index[x] == "rank":
                     rank = ranklist[i]
                 if index[x] == "Category":
                     categories.append(ranklist[i])
                 if index[x] == "Likes": #Sort and make data here
-                    suml += int(a)
+                    if int(a) == 0:
+                        c += 1
+                    else:
+                        suml += int(a)
                     if int(a) > like[0]:
-                        like = [int(a),df.loc[int(rank)-1]]
+                        like = [int(a),df.loc[int(rank)+1]]
                 if index[x] == "Dislikes":
-                    sumd += int(a)
+                    if int(a) == 0:
+                        cc += 1
+                    else:
+                        sumd += int(a)
                     if int(a) > dislike[0]:
-                        dislike = [int(a),df.loc[int(rank)-1]]
+                        dislike = [int(a),df.loc[int(rank)=1]]
                 if index[x] == "Video views":
-                    sumv += int(a)
+                    if int(a) == 0:
+                        ccc += 1
+                    else:
+                        sumv += int(a)
                     if int(a) > view[0]:
-                        view = [int(a),df.loc[int(rank)-1]]
+                        view = [int(a),df.loc[int(rank)]]
 
                 i += 1
         x += 1
-    md = sumd / l
+    cat = max(set(categories), key=categories.count) #this means that music videos are most likely to be trending
+    cat1 = categories.count(cat)
+    md = sumd / l #mean of dislikes, likes, and views
     ml = suml / l
     mv = sumv / 1
+
+    print("Here is the top category out of all 1000 videos, and the amount of videos")
+    print(cat1)
+    print(cat)
+    print("here are the means of the views, likes, and dislikes")
     print(mv)
     print(ml)
     print(md)
-    print(df.loc[5])
-    print(view)
+    print("here are the videos with the highest views,likes, and dislikes in that order")
+    print(view[0])
+    print(view[1])
+    print(like[0])
+    print(like[1])
+    print(dislike[0])
+    print(dislike[1])
 process(df)
